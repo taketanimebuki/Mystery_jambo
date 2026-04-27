dc7bc1de798f4458d3fbffeb984be53f36f71f51
taketani mebuki <taketani@jambo-ine.io>
2026-04-27 12:51:45 +0900

    レビュー内容の修正

--- Assets/Scripts/CharacterPanelChanger.cs
+++ Assets/Scripts/CharacterPanelChanger.cs
@@ -2,39 +2,74 @@ using UnityEngine;
 
 public class CharacterPanelChanger : MonoBehaviour
 {
+    public enum CharacterType
+    {
+        EngineerA,
+        EngineerB,
+        EngineerC,
+        EngineerD,
+        EngineerE,
+        EngineerF,
+        MarketerH,
+        MarketerG,
+        HRI,
+        CEO
+    }
+    
     [System.Serializable]
     public class CharacterPanel
     {
-        public string characterName;
+        public CharacterType characterType;
         public GameObject panel;
     }
 
     public CharacterPanel[] characterPanels;
 
-    void Start()
+    void Awake()
     {
         HideAllPanels();
     }
 
-    public void ShowPanel(string characterName)
+    public void ShowPanel(CharacterType type)
     {
         HideAllPanels();
 
         foreach(CharacterPanel cp in characterPanels)
         {
-            if(cp.characterName == characterName)
+            if(cp.panel == null)
+            {
+                continue;
+            }
+            
+            if(cp.characterType == type)
             {
                 cp.panel.SetActive(true);
                 return;
             }
         }
-        Debug.LogWarning("該当するパネルがありません: " + characterName);
+        Debug.LogWarning("該当するパネルがありません: " + type);
     }
+
+    public void ShowPanelByIndex(int index)
+    {
+        if(index < 0 || index >= System.Enum.GetValues(typeof(CharacterType)).Length)
+        {
+            Debug.LogWarning("無効なインデックス:" + index);
+            return;
+        }
+
+        ShowPanel((CharacterType)index);
+    }
+
 
     public void HideAllPanels()
     {
         foreach(CharacterPanel cp in characterPanels)
-        cp.panel.SetActive(false);
+        {
+            if(cp.panel != null)
+            {
+                cp.panel.SetActive(false);
+            }
+        }
     }
-    
 }
